dfcf1f3c2b14d978eeeef2485a55429c0235ad49
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-05-27 10:44:08 +0900

    java의 정석: chap01 - 자바 시작하기

---
+++ chap01/src/chap01/Hello2.java
@@ -0,0 +1,10 @@
+﻿package chap01;
+
+public class Hello2{
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+		//run
+		//ctrl + F11
+	}
+
+}
---
+++ chap01/src/chap01/HelloWorld.java
@@ -0,0 +1,16 @@
+﻿package chap01;
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+	
+	//run
+	//ctrl + F11
+}
